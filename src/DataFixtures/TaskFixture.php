c9530de6460ba5cc7e2f7449daa5e720cc98baaa
Tomáš Reinert <reinerttomas@gmail.com>
2021-12-12 12:55:48 +0100

    fixtures

---
+++ src/DataFixtures/TaskFixture.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class TaskFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            $task = new Task($faker->sentence(5));
+
+            $manager->persist($task);
+            $manager->flush();
+        }
+    }
+}
