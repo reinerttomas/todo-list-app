f83f7d28924bfc1a2dcf854d923d397058296a89
Tomáš Reinert <reinerttomas@gmail.com>
2021-12-26 10:29:06 +0100

    Json

---
+++ src/Exception/Runtime/JsonException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception\Runtime;
+
+use App\Exception\Exception;
+
+final class JsonException extends Exception
+{
+}
---
+++ src/Utils/Json.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utils;
+
+use App\Exception\Runtime\JsonException;
+use Throwable;
+
+class Json
+{
+    /**
+     * @throws JsonException
+     */
+    public static function encode(array $array): string
+    {
+        try {
+            return json_encode($array, JSON_THROW_ON_ERROR);
+        } catch (Throwable $e) {
+            throw new JsonException($e->getMessage());
+        }
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public static function decode(string $json): array
+    {
+        try {
+            return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+        } catch (Throwable $e) {
+            throw new JsonException($e->getMessage());
+        }
+    }
+
+    public static function validate(string $json): bool
+    {
+        $array = json_decode($json, true);
+
+        if ($array === null) {
+            return false;
+        }
+
+        return true;
+    }
+}
